36dce74229bcbacae82d36ca86cf7bdb27393e6f
Aru <aru.yelemes23@gmail.com>
2020-10-03 16:58:01 +0600

    Second commit

--- src/main/java/kz/iitu/shop/ShopApplication.java
+++ src/main/java/kz/iitu/shop/ShopApplication.java
@@ -2,8 +2,12 @@ package kz.iitu.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class ShopApplication {
 
 	public static void main(String[] args) {
