2958f447356517f2895d69d6c81cb8cd29f9ca85
Aru <aru.yelemes23@gmail.com>
2020-10-12 14:36:23 +0600

    Add functionality

--- src/main/java/kz/iitu/shop/controller/ProductController.java
+++ src/main/java/kz/iitu/shop/controller/ProductController.java
@@ -1,36 +1,51 @@
 package kz.iitu.shop.controller;
-import kz.iitu.shop.exception.ProductNotFoundException;
-import kz.iitu.shop.models.Product;
-import kz.iitu.shop.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import kz.iitu.shop.models.Product;
+import kz.iitu.shop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/products")
-
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
-    private final ProductService productService;
+    private final Logger log = LoggerFactory.getLogger(ProductController.class);
+
 
     @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
+    public ProductRepository productRepos;
+
+    @GetMapping("/list")
+    Collection<Product> getProducts() {
+        return productRepos.findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
+    @GetMapping("/{id}")
+    ResponseEntity<?> getProduct(@PathVariable Long id) {
+        Optional<Product> product = productRepos.findById(id);
+        return product.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @GetMapping("")
-    public List<Product> allProducts() {
-        return productService.findAllProducts();
+
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productRepos.deleteById(id);
     }
 
-    @GetMapping("/{code}")
-    public Product productByCode(@PathVariable String code) {
-        return productService.findProductByCode(code)
-                .orElseThrow(() -> new ProductNotFoundException("Product with code ["+code+"] doesn't exist"));
-    }
 }
